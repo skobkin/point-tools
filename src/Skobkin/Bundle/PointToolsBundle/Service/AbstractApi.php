c53416805839804c23d6be1a96ba414a1aef1746
Alexey Skobkin <x@skobkin.ru>
2015-03-24 02:41:41 +0300

    Test implementation of Point API services. Login field added to the User entity.

---
+++ src/Skobkin/Bundle/PointToolsBundle/Service/AbstractApi.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Skobkin\Bundle\PointToolsBundle\Service;
+
+use Guzzle\Service\Client;
+use Guzzle\Http\Message\Request as GuzzleRequest;
+use Guzzle\Http\Message\Response as GuzzleResponse;
+
+// @todo Implement commands: https://github.com/misd-service-development/guzzle-bundle/blob/master/Resources/doc/serialization.md
+class AbstractApi
+{
+    /**
+     * @var Client HTTP-client from Guzzle
+     */
+    protected $client;
+    
+    public function __construct($httpClient)
+    {
+        $this->client = $httpClient;
+    }
+    
+    /**
+     * Make GET request and return Response object
+     * 
+     * @param string $pathTemplate
+     * @param array $parameters
+     * @return GuzzleResponse
+     */
+    public function sendGetRequest($pathTemplate, array $parameters = [])
+    {
+        $path = vsprintf($pathTemplate, $parameters);
+        
+        $request = $this->client->get($path);
+        
+        return $request->send();
+    }
+}
