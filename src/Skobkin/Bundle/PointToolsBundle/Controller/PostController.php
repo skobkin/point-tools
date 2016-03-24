f0ed72aa366c9319a2ae9596af5a20a5102131ce
Alexey Skobkin <x@skobkin.ru>
2016-03-25 01:10:10 +0300

    Basic post page implemented.

---
+++ src/Skobkin/Bundle/PointToolsBundle/Controller/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Skobkin\Bundle\PointToolsBundle\Controller;
+
+use Skobkin\Bundle\PointToolsBundle\Entity\Blogs\Post;
+use Skobkin\Bundle\PointToolsBundle\Service\UserApi;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
+
+class PostController extends Controller
+{
+    /**
+     * @ParamConverter("post", class="SkobkinPointToolsBundle:Blogs\Post")
+     *
+     * @return Response
+     */
+    public function showAction(Post $post)
+    {
+        $userApi = $this->container->get('skobkin_point_tools.api_user');
+
+        return $this->render('SkobkinPointToolsBundle:Post:show.html.twig', [
+            'post' => $post,
+            'avatar_url' => $userApi->getAvatarUrl($post->getAuthor(), UserApi::AVATAR_SIZE_LARGE),
+        ]);
+    }
+
+}
