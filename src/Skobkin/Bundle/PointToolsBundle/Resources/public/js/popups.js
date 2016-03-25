c4182e3621db444cdfd1f142e9acfa8c32508144
Alexey Skobkin <x@skobkin.ru>
2016-03-25 17:06:14 +0300

    Post page layout. New markdown parser used for post text rendering. Lightbox analog library integrated.

---
+++ src/Skobkin/Bundle/PointToolsBundle/Resources/public/js/popups.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    // Using http://dimsemenov.com/plugins/magnific-popup/documentation.html
+    $('.post-image').magnificPopup({
+        type:'image',
+        gallery: {
+            enabled: true,
+        }
+    });
+});
