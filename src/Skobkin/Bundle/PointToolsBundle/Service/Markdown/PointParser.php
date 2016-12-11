6d8e17b1145cce3765fe6a326cc9956297271c8a
Alexey Skobkin <x@skobkin.ru>
2016-12-11 03:20:55 +0300

    composer update. PointParser updated to match new version of markdown library.

--- src/Skobkin/Bundle/PointToolsBundle/Service/Markdown/PointParser.php
+++ src/Skobkin/Bundle/PointToolsBundle/Service/Markdown/PointParser.php
@@ -73,35 +73,27 @@ class PointParser extends MarkdownParser
 
     /**
      * Point.im breaks Markdown double line wrap rule
-     *
-     * @param $text
-     *
-     * @return mixed
+     * {@inheritdoc}
      */
-    protected function formParagraphs($text) {
-        #
-        #	Params:
-        #		$text - string to process with html <p> tags
-        #
-        # Strip leading and trailing lines:
+    protected function formParagraphs($text, $wrap_in_p = true) {
+        // Strip leading and trailing lines:
         $text = preg_replace('/\A\n+|\n+\z/', '', $text);
 
         $grafs = preg_split('/\n+/', $text, -1, PREG_SPLIT_NO_EMPTY);
 
-        #
-        # Wrap <p> tags and unhashify HTML blocks
-        #
+        // Wrap <p> tags and unhashify HTML blocks
         foreach ($grafs as $key => $value) {
             if (!preg_match('/^B\x1A[0-9]+B$/', $value)) {
-                # Is a paragraph.
+                // Is a paragraph.
                 $value = $this->runSpanGamut($value);
-                $value = preg_replace('/^([ ]*)/', "<p>", $value);
-                $value .= "</p>";
+                if ($wrap_in_p) {
+                    $value = preg_replace('/^([ ]*)/', "<p>", $value);
+                    $value .= "</p>";
+                }
                 $grafs[$key] = $this->unhash($value);
-            }
-            else {
-                # Is a block.
-                # Modify elements of @grafs in-place...
+            } else {
+                // Is a block.
+                // Modify elements of @grafs in-place...
                 $graf = $value;
                 $block = $this->html_hashes[$graf];
                 $graf = $block;
