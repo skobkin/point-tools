d988bac7b07e17926d750b1d2afd75ee577b961b
Alexey Skobkin <x@skobkin.ru>
2016-12-12 02:58:11 +0300

    MainControllerTest now checks non-existing user search scenario.

--- src/Skobkin/Bundle/PointToolsBundle/Tests/Controller/MainControllerTest.php
+++ src/Skobkin/Bundle/PointToolsBundle/Tests/Controller/MainControllerTest.php
@@ -9,16 +9,44 @@ class MainControllerTest extends WebTestCase
     public function testUserSearch()
     {
         $client = static::createClient();
-
         $crawler = $client->request('GET', '/');
 
         $userSearchForm = $crawler->filter('form.form-inline')->form();
-
         $userSearchForm['skobkin_bundle_pointtoolsbundle_user_search[login]'] = 'testuser';
 
         $crawler = $client->submit($userSearchForm);
 
         $this->assertTrue($client->getResponse()->isRedirect('/user/testuser'), 'Redirect to testuser\'s page didn\'t happen');
+    }
+
+    public function testNonExistingUserSearch()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $userSearchForm = $crawler->filter('form.form-inline')->form();
+        $userSearchForm['skobkin_bundle_pointtoolsbundle_user_search[login]'] = 'non-existing-user';
+
+        $crawler = $client->submit($userSearchForm);
+
+        $this->assertFalse($client->getResponse()->isRedirection(), 'Redirect to non-existing user on the main page');
+
+        $formElement = $crawler->filter('form.form-inline')->first();
+
+        $this->assertEquals(1, $formElement->count(), 'Form not found after searching non-existing user');
+
+        $loginInputElement = $formElement->filter('#skobkin_bundle_pointtoolsbundle_user_search_login')->first();
+
+        $this->assertEquals(1, $loginInputElement->count(), 'Login form input element not found after search of non existing user');
+
+        $errorsListElement = $loginInputElement->siblings()->filter('span.help-block')->children()->filter('ul.list-unstyled')->first();
+
+        $this->assertEquals(1, $errorsListElement->count(), 'Form errors list not found after search of non-existing user');
+
+        $firstError = $errorsListElement->children()->first();
+
+        $this->assertEquals(1, $firstError->count(), 'No errors in the list');
+        $this->assertEquals(' Login not found', $firstError->text(), 'Incorrect error text');
     }
 
     public function testUserStats()
