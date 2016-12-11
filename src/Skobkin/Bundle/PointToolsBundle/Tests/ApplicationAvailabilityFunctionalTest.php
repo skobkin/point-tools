3d2a1593cf3d9936c4deaf2974428d6aac369458
Alexey Skobkin <x@skobkin.ru>
2016-12-11 08:26:30 +0300

    First functional test.

---
+++ src/Skobkin/Bundle/PointToolsBundle/Tests/ApplicationAvailabilityFunctionalTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Skobkin\Bundle\PointToolsBundle\Tests;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApplicationAvailabilityFunctionalTest extends WebTestCase
+{
+    /**
+     * @dataProvider urlProvider
+     */
+    public function testPageIsSuccessful($url)
+    {
+        $client = self::createClient();
+        $client->request('GET', $url);
+
+        $this->assertTrue($client->getResponse()->isSuccessful());
+    }
+
+    public function urlProvider()
+    {
+        return [
+            ['/'],
+            ['/users/top'],
+            ['/events/last'],
+            ['/user/testuser']
+        ];
+    }
+}
