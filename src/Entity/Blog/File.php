fa9a84f2a7a43a3b46031ece505a09a169810656
Alexey Skobkin <skobkin-ru@ya.ru>
2023-03-19 17:18:19 +0300

    Ported Blog\File entity and repo.

---
+++ src/Entity/Blog/File.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity\Blog;
+
+use App\Entity\Blog\Post;
+use App\Repository\Blog\FileRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FileRepository::class)]
+#[ORM\Table(name: 'files', schema: 'posts')]
+class File
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(name: 'id', type: 'integer')]
+    private ?int $id;
+
+    #[ORM\Column(name: 'remote_url', type: 'text', unique: true)]
+    private string $remoteUrl;
+
+
+    public function __construct($remoteUrl)
+    {
+        $this->remoteUrl = $remoteUrl;
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getRemoteUrl(): string
+    {
+        return $this->remoteUrl;
+    }
+}
---
+++ src/Repository/Blog/FileRepository.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repository\Blog;
+
+use App\Entity\Blog\File;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<File>
+ *
+ * @method File|null find($id, $lockMode = null, $lockVersion = null)
+ * @method File|null findOneBy(array $criteria, array $orderBy = null)
+ * @method File[]    findAll()
+ * @method File[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class FileRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, File::class);
+    }
+
+    public function save(File $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
